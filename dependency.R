8d2a192296a814907e3ea78e326aa8c9df63a21c
Anudeep Reddy Bommireddy <anudeep1993bommi@gmail.com>
2018-12-14 12:07:33 +0530

    Add files via upload

---
+++ dependency.R
@@ -0,0 +1,9 @@
+try(require("shiny")||install.packages("shiny"))
+try(require("nFactors")||install.packages("nFactors"))
+try(require("qgraph")||install.packages("qgraph"))
+try(require("corrplot")||install.packages("corrplot"))
+
+library("shiny")
+library("nFactors")
+library("qgraph")
+library("corrplot")
